dac9450e91d4f38bd07c65807b2a67a796a6e875
fatehbirbains079 <fatehbir.bains1@rbe.sk.ca>
2026-01-11 03:29:09 +0530

    Version 3
    Added initial project details and room structure.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hi")
+# CS30 Final Project 
+# Version 3: Movement + 3 Chances + Math Rooms 
+
+import random 
+
+rooms = {
+    "Lobby": "{ 
+        "description": "You are in the lobby. The doors are locked.", 
+        "exits": {"west": "Lobby", "north": "Office", "east": "Stairs"}, 
