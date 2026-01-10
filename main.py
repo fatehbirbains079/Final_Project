3fe8dae742d4bafec2c06da33962ae7a7bdee2e1
fatehbirbains079 <fatehbir.bains1@rbe.sk.ca>
2026-01-11 03:40:01 +0530

    Version 3
    Fixed formatting issues and added missing commas in the rooms dictionary.

--- main.py
+++ main.py
@@ -6,4 +6,35 @@ import random
 rooms = {
     "Lobby": "{ 
         "description": "You are in the lobby. The doors are locked.", 
+        "exits": {"east": "Hallway"}, 
+        "type": "start"
+    }, 
+    "Hallway": {
+        "description": "You are in a hallway. It is quiet and dark.", 
         "exits": {"west": "Lobby", "north": "Office", "east": "Stairs"}, 
+        "type": "normal"
+    }, 
+    "Office": {
+        "description": "You are in an office. There is a keypad on the door.", 
+        "exits": {"west": "Hallway", "north": "Exit"}, 
+        "type": "normal" 
+    }, 
+    "Exit": {
+        "description": "You are at the exit door. You can finally leave.",
+        "exits": {}
+        "type": "exit" 
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
