babd8cf1d23f8081d1e997ec2b921ddfbacc2120
fatehbirbains079 <fatehbir.bains1@rbe.sk.ca>
2026-01-11 02:02:52 +0530

    Version 2

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
-print("Hi")
+# CS30 Final Project
+# Version 2: Map + Player Movement 
+
+rooms = {
+  "Lobby": {
+      "description": "You are in the lobby. The doors are locked.", 
+      "exits": {"east": "Hallway"}, 
+      "type": "start"
+  }, 
+  "Hallway": {
+      "description": "You are in a hallway. It is quiet and dark.", 
+      "exits": {"west": "Lobby", "north": "Office", "east": "Stairs"}, 
+      "type": "normal"
+  }, 
+  "Office": {
+      "description": "You are in an office. There is a keypad on the door.", 
+      "exits": {}
+      "type": "exit"
+  }
+}
+
+current_room = "Lobby"
+
+
+def show_room(room_name): 
+    room = rooms[room_name]
+    print("\nRoom:", room_name)
+    print(room["description"])
+
+    exits = room["exits"].keys() 
+
+
+
+
+
+
+
+
+
+
+
+
