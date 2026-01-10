d4dd4e8b193fee0e5dbdd7b26a9761bf7ed0d939
fatehbirbains079 <fatehbir.bains1@rbe.sk.ca>
2026-01-11 03:03:28 +0530

    Version 2

--- main.py
+++ main.py
@@ -28,9 +28,61 @@ def show_room(room_name):
     print(room["description"])
 
     exits = room["exits"].keys() 
+    if len(room["exits"]) == 0: 
+        print("Exits: none")
+    else:
+        print("Exits:", ", ".join(exits))
+
+
+def get_move(): 
+    while True: 
+        move = input("\nMove (north/south/east/west) or quit: ").lower()
+
+        if move == "quit": 
+            return "quit"
+
+        if move in ["north", "south", "east", "west"]: 
+            return move 
+
+        print("Invalid input. Try again.")
+
+
+def move_player(room_name, direction): 
+    room = rooms[room_name]
+
+    if direction in room["exits"]: 
+        return room["exits"][direction]
+
+    print("You can't go that way.")
+    return room_name 
+
+While True: 
+    show_room(current_room)
+
+    if rooms[current_room]["type"] == "exit": 
+        print("\nYou reached the exit!")
+        break
+
+    choice = get_move()
+
+    if choice == "quit": 
+        print("You quit the game")
+        break
+
+    current_room = move_player(current_room, choice)
 
 
 
+
+
+
+
+
+
+
+
+
+      
 
 
 
