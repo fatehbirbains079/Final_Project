2ff4a816119b6a8a27bc4c2d76709b30e4d791a0
fatehbirbains079 <fatehbir.bains1@rbe.sk.ca>
2026-01-16 01:02:36 +0530

    Version 5
    Added new rooms and descriptions to the game.

--- main.py
+++ main.py
@@ -24,4 +24,69 @@ rooms = {
         "exits": {"west": "Hallway"}, 
         "type": "normal"
     }, 
-    "Stairs "
+    "Stairs Up": {
+        "description": "You are at the stairs going up to the second floor.", 
+        "exits": {"north": "Second Hallway", "northwest": "Second Hallway"}, 
+        "type": "normal"
+    }, 
+    "Second Hallway": {
+        "description": "You are at the stairs going back down.", 
+        "exits": {"south": "Stairs Down", "east": "Security Room", "west": "Break Room"}, 
+        "type": "normal"
+    }, 
+    "Stairs Down": {
+        "description": "You are in the break room. Nothing useful here.", 
+        "exits": {"north": "Second Hallway", "south": "Hallway"}, 
+        "type": "normal"
+    }, 
+    "Break Room": {
+        "description": "You are in the break room. Nothing useful here.", 
+        "exits": {"west": "Second Hallway", "north": "Rooftop Exit"}, 
+        "type": "normal"
+    }, 
+    "Security Room": { 
+        "description": "You are in the security room. The exit is here.",
+        "exits": {"west": "Second Hallway", "north: "Rooftop Exit"}, 
+        "type": "math"
+    }
+}
+
+current_room = "Lobby"
+chances = 3
+
+
+def show_room(room_name): 
+    room = rooms[room_name]
+    print("\nRoom:", room_name)
+    print(room["description"])
+
+    if room["exits"]: 
+        print("Exits:", ", ".join(room["exits"].keys()))
+    else:
+        print("Exits:none")
+
+def get_move(): 
+    while True:
+        move = input("\nMove (north/south/east/west) or quit: ").lower() 
+
+        if move == "quit": 
+            return "quit" 
+
+        if move in ["north", "south", "east", "west"]: 
+            return move 
+
+        print("Invalid input. Try again.")
+
+
+def ask_math_question(): 
+    operation = random.choice(["+", "-", "*"])
+
+    num1 = random.randint(1, 10)
+    num2 = random.randint(1, 10)
+
+    if operation == "-": 
+        if num1, num2 = num2, num1 
+    correct = num1 - num2 
+    question = f"{num1} * {num2}" 
+
+    elif operation 
