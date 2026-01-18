388dd3a035fa5b9d7279606a23c6bcf9fc02d782
fatehbirbains079 <fatehbir.bains1@rbe.sk.ca>
2026-01-19 03:37:44 +0530

    Version 7

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Hi")
+# CS30 Final Project 
+# Version 7: Add Enemy 
+
+import random 
+
+rooms = { 
+    "Lobby": { 
+        "description": "You are in the lobby. The doors are locked.", 
+        "exits": {"east": "Hallway"}, 
+        "type": "start"
+    }, 
+    "Hallway": {
+        "description": "You are in a hallway. It is quiet and dark.", 
+        "exits": {"west": "Lobby", "north": "Office", "east": "Storage", "south": "Stairs Up"}, 
+        "type": "math"
+    }, 
+    "Storage": {
+        "description": "You are in a storage room. Boxes are everywhere.", 
+        "exits": {"west": "Hallway"}, 
+        "type": "normal"
+    }, 
+    "Stairs Up": {
+        "description": "You are at the stairs going up to the second floor.",
+        "exits": {"north": "Second Hallway"}, 
+        "type": "normal"
+    }, 
+    "Second Hallway": { 
+        "description": "You are on the second flooor "
